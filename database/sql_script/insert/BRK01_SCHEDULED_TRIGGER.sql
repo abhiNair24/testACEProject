6673d4ee3aa00165acb3469b9215b6e479dfd849
Jimmy Lindau <JIMA@lundbeck.com>
2021-08-19 12:09:06 +0000

    Updated
    git-svn-id: https://hluw2917p/svn/MiddleWare/IC/IIB/Integrations/INT0051-INT0100/INT0080_HRDB/trunk@8838 b8dfa711-d165-4148-96ac-315d517ec26d

--- database/sql_script/insert/BRK01_SCHEDULED_TRIGGER.sql
+++ database/sql_script/insert/BRK01_SCHEDULED_TRIGGER.sql
@@ -12,7 +12,7 @@
 * Development Environment
 *******************************************************************
 Insert into IIBUSER.BRK01_SCHEDULED_TRIGGER (TRIGGERID,TRIGGERFLOWNAME,TRIGGERQUEUE,STARTTIME,NEXTSCHEDULE,YEAR,MONTH,DAY,HOUR,MINUT,ACTIVE) values ('''SF_To_HRDB_OrgData'',''Sequential''','INT0080.SF_GET.FO_COMPANY.MAIN','INT0080.GET.FO.COMPANY.TRIGGER',to_timestamp('2021-08-19 15:45:00','RRRR-MM-DD HH24:MI:SS'),null,null,null,null,'3',null,'No');
-Insert into IIBUSER.BRK01_SCHEDULED_TRIGGER (TRIGGERID,TRIGGERFLOWNAME,TRIGGERQUEUE,STARTTIME,NEXTSCHEDULE,YEAR,MONTH,DAY,HOUR,MINUT,ACTIVE) values ('''SF_To_HLuPersonnel2_EmployeeData'',''Sequential''','INT0080.SF_GET.PERPERSON.MAIN','INT0080.GET.PERPERSON.TRIGGER',to_timestamp('2021-08-19 16:00:00','RRRR-MM-DD HH24:MI:SS'),null,null,null,null,'3',null,'No');
+Insert into IIBUSER.BRK01_SCHEDULED_TRIGGER (TRIGGERID,TRIGGERFLOWNAME,TRIGGERQUEUE,STARTTIME,NEXTSCHEDULE,YEAR,MONTH,DAY,HOUR,MINUT,ACTIVE) values ('''SF_To_HRDB_EmployeeData'',''Sequential''','INT0080.SF_GET.PERPERSON.MAIN','INT0080.GET.PERPERSON.TRIGGER',to_timestamp('2021-08-19 16:00:00','RRRR-MM-DD HH24:MI:SS'),null,null,null,null,'3',null,'No');
 
 *******************************************************************
 * Test/QA Environment
