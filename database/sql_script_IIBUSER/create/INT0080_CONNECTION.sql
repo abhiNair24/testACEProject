2340a54d9ff3fd44fbe9643f5c2f90b827b3c9d6
Jimmy Lindau <JIMA@lundbeck.com>
2023-11-20 08:03:14 +0000

    Sorted DB scripts for ACE migration
    git-svn-id: https://hluw2917p/svn/MiddleWare/IC/ACE/Integrations/INT0080_HRDB/trunk@12075 b8dfa711-d165-4148-96ac-315d517ec26d

--- database/sql_script_IIBUSER/create/INT0080_CONNECTION.sql
+++ database/sql_script_IIBUSER/create/INT0080_CONNECTION.sql
@@ -1,9 +1,9 @@
--- ************************
--- * Author               : Klaus Johansen (KLJ)
--- * History              : 2022-04-06 Table structure for oAuth/SAML authentication 
--- * Description          : Contains values needed to login to SF/PP oData interface
--- *                      : 
--- ************************
+-- Revision             : $Id$
+-- Script               : $URL$
+
+set echo on
+whenever sqlerror exit failure
+
 --------------------------------------------------------
 --  DDL for Table INT0080_MISC
 --------------------------------------------------------
@@ -23,3 +23,5 @@ ALTER TABLE INT0080_CONNECTION  MODIFY (CLIENT_ID NOT NULL);
 ALTER TABLE INT0080_CONNECTION  MODIFY (COMPANY_ID NOT NULL);
 ALTER TABLE INT0080_CONNECTION  MODIFY (SAML_ASSERTION NOT NULL);
 CREATE UNIQUE INDEX INT0080_CONNECTION_INDEX1 ON INT0080_CONNECTION (BASEURL, CLIENT_ID, COMPANY_ID);
+
+EXIT
