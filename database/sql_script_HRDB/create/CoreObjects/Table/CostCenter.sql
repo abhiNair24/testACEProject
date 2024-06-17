09bd9107a7de7ece0a10090f4f2095a478d99584
Jimmy Lindau <JIMA@lundbeck.com>
2024-06-17 12:19:33 +0000

    Updated
    git-svn-id: https://hluw2917p/svn/MiddleWare/IC/ACE/Integrations/INT0080_HRDB/trunk@12933 b8dfa711-d165-4148-96ac-315d517ec26d

--- database/sql_script_HRDB/create/CoreObjects/Table/CostCenter.sql
+++ database/sql_script_HRDB/create/CoreObjects/Table/CostCenter.sql
@@ -10,15 +10,14 @@ GO
 CREATE TABLE [dbo].[CostCenter](
 	[costcenterId] [nvarchar](32) NOT NULL,
 	[name] [nvarchar](90) NULL,
+	[locationId] [nvarchar](32) NULL,
 	[managerUserId] [nvarchar](100) NULL,
 	[status] [nvarchar](2) NULL,
 	[recordLastUpdated] [datetime] NULL,
 	[lastSyncDate] [datetime] NULL,
- CONSTRAINT [PK_CostCenter] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_tblCostctr] PRIMARY KEY CLUSTERED 
 (
 	[costcenterId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-
-
--- database/sql_script_HRDB/create/CoreObjects/Table/OrgUnit.sql
+++ database/sql_script_HRDB/create/CoreObjects/Table/OrgUnit.sql
@@ -12,6 +12,7 @@ CREATE TABLE [dbo].[OrgUnit](
 	[name] [nvarchar](90) NULL,
 	[orgUnitType] [nvarchar](20) NULL,
 	[orgUnitParent] [nvarchar](32) NULL,
+	[locationId] [nvarchar](32) NULL,
 	[costCenterId] [nvarchar](32) NULL,
 	[headOfUnitUserId] [nvarchar](100) NULL,
 	[status] [nvarchar](2) NULL,
@@ -23,5 +24,3 @@ CREATE TABLE [dbo].[OrgUnit](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-
-
